72044da0c9ac6d1fb229aa6fdc1b0ed880200916
parker skarecky <taicundnhs@gmail.com>
2019-10-03 14:59:51 -0700

    10/3f

--- questions/questionDriver.c
+++ questions/questionDriver.c
@@ -8,7 +8,7 @@
 
 #include "questions.h"
 struct mainSection{
-    int score, questions;
+    int score,questions;
     int sectionScore[5];
     int sectionQuestions[5];
     
@@ -22,73 +22,81 @@ struct mathSection{
 int questionDriver(char *message) {
     s.score = 0;
     s.questions = 0;
-    s.sectionQuestions[5]={0,0,0,0,0};
+    for (int i=0;i<5;i++) {
+        s.sectionQuestions[i]=0;
+        //printf("%i",i);
+    }
+    for (int i=0;i<5;i++) {
+        s.sectionScore[i]=0;
+        //printf("%i",i);
+    }
+    //s.sectionQuestions[5]={0,0,0,0,0};
     srand((uint)time(0));
     
     { // Math
-        *m.sectionName = "Math";
+        m.sectionName = "Math";
         m.sNum = 0;       // section number
         m.randCtrl = 5 + 1;
         
-        s.sectionQuestions[m.sNum] = rand() % m.randCtrl;  s.questions += s.sectionQuestions[sNum];
-        s.sectionScore[sNum] = mathQuestions(s.sectionQuestions[m.sNum]); s.score += s.sectionScore[sNum];
-        printf("Your %s score is %d out of %d\n\n", m.sectionName, s.sectionScore[sNum], sectionQuestions[sNum] );
+        s.sectionQuestions[m.sNum] = rand() % m.randCtrl;  s.questions += s.sectionQuestions[m.sNum];
+        s.sectionScore[m.sNum] = mathQuestions(s.sectionQuestions[m.sNum]); s.score += s.sectionScore[m.sNum];
+        printf("Your %s score is %d out of %d\n\n", m.sectionName, s.sectionScore[m.sNum], s.sectionQuestions[m.sNum] );
     }
     
     { // Binary Math
-        char *sectionName = "Binary";
-        int sNum = 1;       // section number
-        int randCtrl = 0 + 1;
+        m.sectionName = "Binary";
+        m.sNum = 1;       // section number
+        m.randCtrl = 0 + 1;
         
-        sectionQuestions[sNum] = rand() % randCtrl;  questions += sectionQuestions[sNum];
-        // sectionScore[sNum] = binaryQuestions(sectionQuestions[sNum]); score += sectionScore[sNum];
-        printf("Your %s score is %d out of %d\n\n", sectionName, sectionScore[sNum], sectionQuestions[sNum] );
+        s.sectionQuestions[m.sNum] = rand() % m.randCtrl; s.questions += s.sectionQuestions[m.sNum];
+        //s.sectionScore[m.sNum] = binaryQuestions(s.sectionQuestions[m.sNum]); score +=s.sectionScore[m.sNum];
+        printf("Your %s score is %d out of %d\n\n", m.sectionName,s.sectionScore[m.sNum], s.sectionQuestions[m.sNum] );
     }
     
     { // Assignment operators
-        char *sectionName = "Assignment";
-        int sNum = 1;       // section number
-        int randCtrl = 0 + 1;
+        m.sectionName = "Assignment";
+        m.sNum = 1;       // section number
+        m.randCtrl = 0 + 1;
         
-        sectionQuestions[sNum] = rand() % randCtrl;  questions += sectionQuestions[sNum];
-        // sectionScore[sNum] = assignmentQuestions(sectionQuestions[sNum]); score += sectionScore[sNum];
-        printf("Your %s score is %d out of %d\n\n", sectionName, sectionScore[sNum], sectionQuestions[sNum] );
+        s.sectionQuestions[m.sNum] = rand() % m.randCtrl; s.questions += s.sectionQuestions[m.sNum];
+        //s.sectionScore[m.sNum] = assignmentQuestions(s.sectionQuestions[m.sNum]); score +=s.sectionScore[m.sNum];
+        printf("Your %s score is %d out of %d\n\n", m.sectionName,s.sectionScore[m.sNum], s.sectionQuestions[m.sNum] );
     }
     
     { // Expressions
-        char *sectionName = "Expressions";
-        int sNum = 2;       // section number
-        int randCtrl = 0 + 1;
+        m.sectionName = "Expressions";
+        m.sNum = 2;       // section number
+        m.randCtrl = 0 + 1;
         
-        sectionQuestions[sNum] = rand() % randCtrl;  questions += sectionQuestions[sNum];
-        // sectionScore[sNum] = expressionQuestions(sectionQuestions[sNum]); score += sectionScore[sNum];
-        printf("Your %s score is %d out of %d\n\n", sectionName, sectionScore[sNum], sectionQuestions[sNum] );
+        s.sectionQuestions[m.sNum] = rand() % m.randCtrl; s.questions += s.sectionQuestions[m.sNum];
+        //s.sectionScore[m.sNum] = expressionQuestions(s.sectionQuestions[m.sNum]); score +=s.sectionScore[m.sNum];
+        printf("Your %s score is %d out of %d\n\n", m.sectionName,s.sectionScore[m.sNum], s.sectionQuestions[m.sNum] );
     }
     
     { // Logic and Negation
-        char *sectionName = "Logic";
-        int sNum = 3;       // section number
-        int randCtrl = 5 + 1;
+        m.sectionName = "Logic";
+        m.sNum = 3;       // section number
+        m.randCtrl = 5 + 1;
         
-        sectionQuestions[sNum] = rand() % randCtrl;  questions += sectionQuestions[sNum];
-        sectionScore[sNum] = logicQuestions(sectionQuestions[sNum]); score += sectionScore[sNum];
-        printf("Your %s score is %d out of %d\n\n", sectionName, sectionScore[sNum], sectionQuestions[sNum] );
+        s.sectionQuestions[m.sNum] = rand() % m.randCtrl; s.questions += s.sectionQuestions[m.sNum];
+        s.sectionScore[m.sNum] = logicQuestions(s.sectionQuestions[m.sNum]); s.score +=s.sectionScore[m.sNum];
+        printf("Your %s score is %d out of %d\n\n", m.sectionName,s.sectionScore[m.sNum], s.sectionQuestions[m.sNum] );
     }
     
     { // Data Type Conversion
-        char *sectionName = "Conversion";
-        int sNum = 4;       // section number
-        int randCtrl = 0 + 1;
+        m.sectionName = "Conversion";
+        m.sNum = 4;       // section number
+        m.randCtrl = 0 + 1;
         
-        sectionQuestions[sNum] = rand() % randCtrl;  questions += sectionQuestions[sNum];
-        // sectionScore[sNum] = conversionQuestions(sectionQuestions[sNum]); score += sectionScore[sNum];
-        printf("Your %s score is %d out of %d\n\n", sectionName, sectionScore[sNum], sectionQuestions[sNum] );
+        s.sectionQuestions[m.sNum] = rand() % m.randCtrl; s.questions += s.sectionQuestions[m.sNum];
+        //s.sectionScore[m.sNum] = conversionQuestions(s.sectionQuestions[m.sNum]); score +=s.sectionScore[m.sNum];
+        printf("Your %s score is %d out of %d\n\n", m.sectionName,s.sectionScore[m.sNum], s.sectionQuestions[m.sNum] );
     }
     
     // Final Score of test
-    sprintf(message, "%d out of %d", score, questions);
+    sprintf(message, "%d out of %d", s.score,s.questions);
     printf("Your score is %s\n", message);
     
-    return questions;
+    return s.questions;
     
 }
